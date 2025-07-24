9e5710f3b08866ebbb9a1d5fdc38e97980bd6af6
mtouseef265 <mrdesigner265@gmail.com>
2025-07-24 10:17:30 +0500

    edit medusa config

--- medusa-config.ts
+++ medusa-config.ts
@@ -1,6 +1,6 @@
-import { loadEnv, defineConfig } from '@medusajs/framework/utils'
+import { loadEnv, defineConfig } from "@medusajs/framework/utils";
 
-loadEnv(process.env.NODE_ENV || 'development', process.cwd())
+loadEnv(process.env.NODE_ENV || "development", process.cwd());
 
 module.exports = defineConfig({
   projectConfig: {
@@ -12,7 +12,21 @@ module.exports = defineConfig({
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     },
-    databaseDriverOptions: process.env.NODE_ENV !== "development" ?
-      { connection: { ssl: { rejectUnauthorized: false } } } : {},
-  }
-})
+    databaseDriverOptions:
+      process.env.NODE_ENV !== "development"
+        ? { connection: { ssl: { rejectUnauthorized: false } } }
+        : {},
+    admin: {
+      vite: () => {
+        return {
+          server: {
+            allowedHosts: [
+              "https://buddydeals-backend-262420122176.asia-south2.run.app/",
+              "https://buddydeals-backend-uqtppluqga-em.a.run.app",
+            ],
+          },
+        };
+      },
+    },
+  },
+});
